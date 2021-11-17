3df26e69114d3998affc35405012248bb191824c
Alexander Brazda López <alex@ofiben.com>
2021-11-17 14:53:20 +0100

    Add files via upload

---
+++ PrinterIO.pas
@@ -0,0 +1,132 @@
+unit PrinterIO;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.WinSpool, Winapi.Messages,
+  System.Variants, System.Classes,
+  System.SysUtils;
+
+type
+  TPrinterIO=class
+  private
+    FHandle: THandle;   //Printer Handle
+    FJobId: THandle;    //
+
+    FTitle  : String;
+    FEncoding: TEncoding;
+//    FCopies : Integer;
+    function GetVersion():Integer;
+  public
+    property Title : String read FTitle write FTitle;
+//    property Copies:Integer read FCopies write FCopies;
+
+    constructor Create(const APrinterName:String);
+    destructor Destroy();override;
+
+    procedure BeginDoc() overload;
+    procedure BeginDoc(const ATitle:String) overload;
+    procedure EndDoc();
+    procedure Abort();
+
+    function Write(const AValue:String):TPrinterIO;
+    function WriteLn():TPrinterIO overload;
+    function WriteLn(const AValue:String):TPrinterIO overload;
+  end;
+
+implementation
+
+constructor TPrinterIO.Create(const APrinterName:String);
+begin
+  inherited Create;
+
+  if not OpenPrinter(pChar(APrinterName),FHandle,nil) then
+    RaiseLastOSError; // Exception.Create('OpenPrinter('+APrinterName+')');
+  FEncoding:=TEncoding.GetEncoding(850);
+end;
+
+destructor TPrinterIO.Destroy();
+begin
+  inherited;
+
+  ClosePrinter(FHandle);
+  FreeAndNil(FEncoding);
+end;
+
+procedure TPrinterIO.BeginDoc();
+var
+  DOC_INFO : DOC_INFO_1;
+begin
+  DOC_INFO.pDocName:=pChar(FTitle);
+  DOC_INFO.pOutputFile:=nil;
+  DOC_INFO.pDatatype:='RAW';
+  if GetVersion()=4 then
+    DOC_INFO.pDatatype:='XPS_PASS';
+
+  FJobId:=StartDocPrinter(FHandle,1,@DOC_INFO);
+  if FJobId=0 then
+    RaiseLastOSError();
+
+  if not StartPagePrinter(FHandle) then
+    RaiseLastOSError();
+end;
+
+procedure TPrinterIO.BeginDoc(const ATitle: String);
+begin
+  FTitle:=ATitle;
+  BeginDoc();
+end;
+
+procedure TPrinterIO.EndDoc();
+begin
+  EndPagePrinter(FHandle);
+  EndDocPrinter(FHandle);
+end;
+
+function TPrinterIO.GetVersion(): Integer;
+var
+  Buffer: PByte;
+  BufferSize: DWORD;
+begin
+  GetPrinterDriver(FHandle, nil, 2, nil, 0, BufferSize);
+
+  if GetLastError() <> ERROR_INSUFFICIENT_BUFFER then
+    exit(-1);
+
+  Buffer:=GetMemory(BufferSize);
+  if not GetPrinterDriver(FHandle, nil, 2, Buffer, BufferSize, BufferSize) then
+    RaiseLastOSError();
+
+  Result:=PDriverInfo2(Buffer).cVersion;
+end;
+
+procedure TPrinterIO.Abort();
+begin
+  AbortPrinter(FHandle);
+end;
+
+function TPrinterIO.Write(const AValue:String):TPrinterIO;
+var
+  Written : Cardinal;
+  Buffer: TBytes;
+begin
+  Buffer:=FEncoding.GetBytes(AValue);
+  WritePrinter(FHandle,pByte(Buffer),Length(Buffer),Written);
+  Result:=Self;
+end;
+
+function TPrinterIO.WriteLn(const AValue:String):TPrinterIO;
+begin
+  Write(AValue);
+  WriteLn;
+  Result:=Self;
+end;
+
+function TPrinterIO.WriteLn():TPrinterIO;
+begin
+  Write(#13#10);
+  Result:=Self;
+end;
+
+end.
+
---
+++ TestPrinterIO.dpr
@@ -0,0 +1,30 @@
+﻿program TestPrinterIO;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,PrinterIO;
+
+procedure Test(const AName:String);
+var
+  Printer: TPrinterIO;
+begin
+  Printer:=TPrinterIO.Create(AName);
+  Printer.BeginDoc('Test');
+  Printer.WriteLn('Hello world');
+  Printer.EndDoc();
+  FreeAndNil(Printer);
+end;
+
+begin
+  try
+//    Test('Godex EZ-1200 Plus');
+    Test('Microsoft XPS Document Writer');
+
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
